a9569d76d60581475e87fb2609802ef405b1239d
Anushkas <anushkas@winjit.com>
2024-02-11 14:42:22 +0530

    Integrated streamlit code

---
+++ StreamlitApp.py
@@ -0,0 +1,63 @@
+import os
+import json
+import traceback
+import pandas as pd
+from dotenv import load_dotenv
+from src.mcqgenerator.utils import read_file, get_table_data
+import streamlit as st
+from langchain.callbacks import get_openai_callback
+from src.mcqgenerator.MCQGenerator import generate_evaluate_chain
+from src.mcqgenerator.logger import logging
+
+
+with open("/home/anushkas/Documents/MCQ_GEN/Response.json", "r") as file:
+    RESPONSE_JSON = json.load(file)
+
+st.title("MCQ Generator")
+
+with st.form("user_input"):
+    uploaded_file = st.file_uploader("Upload a PDF file or text file")
+    mcq_count = st.number_input("Number of MCQ", min_value=3, max_value=50)
+
+    subject = st.text_input("Insert a subject", max_chars=20)
+    tone = st.text_input("Insert a tone", max_chars=20, placeholder="Simple")
+
+    button = st.form_submit_button("Create MCQ")
+
+    if button and uploaded_file is not None and mcq_count and subject and tone:
+        with st.spinner("Loading..."):
+            try:
+                text = read_file(uploaded_file)
+                with get_openai_callback() as cb:
+                    response = generate_evaluate_chain(
+                        {
+                            "text": text,
+                            "number": mcq_count,
+                            "subject": subject,
+                            "tone": tone,
+                            "response_json": json.dumps(RESPONSE_JSON)
+                        }
+                        )
+            except Exception as e:
+                st.error(e)
+                logging.error("ERROR in logger = ", e)
+            else:
+                print(f"Total Tokens:{cb.total_tokens}")
+                print(f"Prompt Tokens:{cb.prompt_tokens}")
+                print(f"Completion Tokens:{cb.completion_tokens}")
+                print(f"Total Cost:{cb.total_cost}")
+            #  print(f"Prompt Cost:{cb.prompt_cost}")
+                if isinstance(response, dict):
+                    quiz = response.get("quiz", None)
+                    if quit is not None:
+                        table_data = get_table_data(quiz)
+                        if table_data is not None:
+                            df = pd.DataFrame(table_data)
+                            df.index = df.index+1
+                            st.table(df)
+                            st.text_area(label="Review",
+                                         value=response['review'])
+                        else:
+                            st.error("Error in table_data")
+                else:
+                    st.write(response)
